e1719cc90918113627dcf13a06c5726653e146e2
bs10R <bhaveshsagale111@gmail.com>
2024-08-01 01:35:03 +0530

    Add files via upload

---
+++ R introduction.R
@@ -0,0 +1,187 @@
+# To zoom in type: ctrl+=
+# To zoom out type: ctrl+- (don't use '-' from Nump)
+# To output text - This is a comment
+#
+
+"Hello World!"
+
+'Hello world'
+
+#Hello world!
+
+
+# To output numbers
+5  
+
+
+
+
+
+
+
+
+
+
+
+# Variable 
+
+name1 = "Amit"
+name1
+
+
+
+
+name2 = "Neha"
+name = "snehal"
+
+
+3*2    
+
+age = 25
+age
+
+
+age = age+5
+age
+
+num=num+5
+
+6*4
+
+
+text = "Excellent"
+
+paste("Excelr is",text) # Concatenate /Combine text using
+paste("students are",text)
+paste("My name is:",name)
+paste("students are",text,"in class")
+
+
+# Assign same value to multiple variables in one line 
+var1 = var2 = var3 = "red"
+
+# Legal variables names
+
+myvar="Bhavesh"
+my_var = "Yash"
+myVar = "Jagit"
+MYVAR = "Ashutosh"
+myvar2 = "Utkarsh"
+this.year = 2022
+
+
+# Data Types in R
+# Numeric/Float - decimal whole +ve -ve
+
+x = 10
+class(x)
+class(name)
+
+# Integer - whole +ve -ve L
+y = 1000L
+class (y)
+
+#character/string
+x = "R is exciting"
+class (x)
+
+# logical/boolean
+
+x = True # or T, FALSE, F
+class(x)
+x = F
+class(x)
+
+3*5
+3>5
+5>3
+5==5 # comparison operator
+1 !=6 # not equal to
+
+
+# Arithmetic Operators
+x = 20
+y = 3
+
+x+y
+x-y
+x*y
+
+x/y #float/decimal result
+
+x %% y #integer division
+
+x^3 # power or raise to
+
+x %% y # modules
+x
+
+
+2*pi
+pi
+
+??Constants
+
+LETTERS
+letters
+pi
+month.name
+month.abb
+
+
+#Built in math function
+
+max(25,78,10) 
+min(2,20,3)
+sqrt(81)
+abs(-4.5)
+
+
+# Data structures: Vectors, Dataframes
+# Homogeneous Vectors
+# vector of strings
+
+fruits = c("Banana","Apple","Orange","Strawberry")
+class(fruits)
+ #vector of numerical
+n1 = c(1,2,3,4)
+class(n1)
+
+n2 = c(T,F,TRUE,FALSE)
+class(n2)
+
+# Heterogeneous Vectors
+mix = c(81,5.2,TRUE,"Mango",5L,F)
+class(mix)
+
+
+x = c(2,5,8,4)
+y = c(1,9,9,9)
+
+x+y
+
+x*5
+(x+y)*2
+
+#sequence
+1:10
+
+
+1:1000
+40:45
+
+seq(1,50,5)# start value,end value,step/increment/decrement
+seq(1,50,by=3)
+seq(1,10)
+seq(10,1,-2)# default increment/decrement is 1
+seq(10,1)
+
+
+n1 = 1.5:6.3 #default increment value is 1
+n1
+
+n1 = 1.5:6.5
+n1
+
+
+ 
